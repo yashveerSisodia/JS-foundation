02852c21fb103405f17ebb0a93cd3f90d8d9d8fd
yashveerSisodia <sisodia.yash98@gmail.com>
2025-04-01 23:51:48 +0800

    part1 datatypes and operators

---
+++ part1/dataTypes.js
@@ -0,0 +1,10 @@
+const userName = "spiderman";
+// userName = "Batman"; -- not allowed
+
+var score = 100;
+console.log(score);
+score = 87;
+
+let getScore = score;
+console.log(getScore);
+
---
+++ part1/operators.js
@@ -0,0 +1,21 @@
+let isLoggedin = true;
+let isActive = true;
+
+if(isLoggedin && isActive) {
+    console.log("User is active and logged in:"+ true);
+} else {
+    console.log(false);
+}
+
+/*
+-- operators
+&&
+||
+!
+==
+!=
+
+arithmatic
++, -, *, /, %, ** (exponential)
+*/
+console.log(2 ** 3);
