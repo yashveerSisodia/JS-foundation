4121b79e60e34979402a3bb94cae298d9ce00f7e
yashveerSisodia <sisodia.yash98@gmail.com>
2025-04-03 23:54:03 +0800

    part3 conditional challenges

---
+++ part3/conditional-challenges.js
@@ -0,0 +1,22 @@
+// check which no is greater 
+
+let num1 = 5;
+let num2 = 8;
+
+if (num2 > num1) {
+    console.log("num2 is greater than num1");
+} 
+
+
+// check an array is empty or not
+
+let fruits = []; // ["orange", "mango", "apple"];
+
+if (fruits.length === 0) {
+    console.log("array is empty");   
+} else {
+    console.log("array is NOT empty");
+}
+
+
+// boolean check 
