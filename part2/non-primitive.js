a41f7c2a160c5ceda2bb5a2b22209e679adc28e1
yashveerSisodia <sisodia.yash98@gmail.com>
2025-04-03 23:03:08 +0800

    part 2 non-primitives

---
+++ part2/non-primitive.js
@@ -0,0 +1,23 @@
+// object, array
+
+const fruits = ["apple", "mango", "organge"];
+console.log(fruits[1]);
+console.log(fruits);
+console.log(typeof fruits);
+
+const student = {
+    name : "Raju",
+    marks : 87.8,
+    address : {
+        city : "Delhi",
+        pincode : 123432
+    }
+};
+
+console.log(student);
+console.log(student.name);
+console.log(typeof student);
+console.log(student.address.city);
+
+
+
