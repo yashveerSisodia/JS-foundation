746884951dead902bdfa8248626593c950bc6e25
yashveerSisodia <sisodia.yash98@gmail.com>
2025-04-08 16:28:36 +0800

    higher order function, nested function

--- part5/functions.js
+++ part5/functions.js
@@ -27,7 +27,42 @@ const calculateTotal = (price, quantity) => {
  *  here `this` is refering to the context of node, but if you use same code in the browser the output of this context will get changed.
  */
 function testOfThis() {
-    console.log(this);  
+    // console.log(this);  
     
 }
 testOfThis();
+
+
+/**
+ * Higher order functions & nested functions
+ * 
+ * write a func named `processedTeaOrder` that takes another function, `makeTea` as a parameter and 
+ * calls it with the argument `earl grey` and return the result of calling `makeTea`
+ * 
+ * here, we are passing the function as an argument.
+ */
+
+function makeTea(typeOfTea) {
+    return `makeTea: ${typeOfTea}`;
+}
+
+function processTeaOrder(teaFunc) {
+    return teaFunc(`earl grey`);
+}
+
+const tea = processTeaOrder(makeTea);
+// console.log(tea);
+
+/**
+ * write a func named `createTeaMaker` that returns another function. The returned function should take one parameter, `teaType`, and return a message like `"Making green tea"`.
+ * Store the returned function in a variable named `teaMaker` and call it with `green tea`.
+ */
+
+function createTeaMaker() {
+    return function (teaType) {
+        return `Making ${teaType}`;
+    }
+}
+
+let teaMaker = createTeaMaker();
+console.log(teaMaker("green tea"));
