30b1a5af5024c67657c49c9b03165b89c9f53c78
yashveerSisodia <sisodia.yash98@gmail.com>
2025-04-11 12:12:19 +0800

    nested function and this

---
+++ coding-exercise/Exercise5.js
@@ -0,0 +1,43 @@
+// nested function and context
+
+
+/**
+ * Nested Functions and Context
+Task 1: Using this in Objects
+
+Create an object person with a method introduce() that uses this
+
+additionally add properties of name & age that will result in Hi, my name is Hitesh and I am 19.5 years old on calling introduce()
+
+*/
+
+
+
+const person = {
+    name: "hitesh",
+    age: 19.5,
+    introduce() {
+        console.log(`Hi, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+};
+
+person.introduce();
+
+
+
+/**
+
+
+Task 2: Function within a function
+
+Write a function outer() that contains another function inner() and returns a value of 'Inner function called' on calling outer()
+ */
+
+
+function outer() {
+    return inner();
+}
+
+const inner = () => `Inner function called`;
+
+console.log(outer());
